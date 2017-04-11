d4a09531f2b411e4407e24da90143b15da41f0bf
shorti1996 <shorti1996@gmail.com>
2017-04-11 23:13:43 +0200

    details activity loads the image

--- app/src/main/java/com/liebert/lab002/MovieDetailsActivity.java
+++ app/src/main/java/com/liebert/lab002/MovieDetailsActivity.java
@@ -4,10 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.liebert.lab002.Models.Movie;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.realm.Realm;
 
 public class MovieDetailsActivity extends AppCompatActivity {
+
+    int movieId;
+    Realm mRealm;
+    Movie mMovie;
 
     public static final String EXTRA_MOVIE = "extra_movie";
 
@@ -19,6 +28,29 @@ public class MovieDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
 
+        mRealm = Realm.getDefaultInstance();
+        movieId = getMovieFromExtra(savedInstanceState);
+
         ButterKnife.bind(this);
+
+        Movie mMovie = mRealm.where(Movie.class).equalTo("id", movieId).findFirst();
+        Glide.with(this)
+                .load(mMovie.getBackdropImageUri())
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.RESULT)
+                .into(backdropIv);
+    }
+
+    private int getMovieFromExtra(Bundle savedInstanceState) {
+        int movieId = -1;
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras != null) {
+                movieId = extras.getInt(EXTRA_MOVIE);
+            }
+        } else {
+             movieId = (int) savedInstanceState.getSerializable(EXTRA_MOVIE);
+        }
+        return movieId;
     }
 }
