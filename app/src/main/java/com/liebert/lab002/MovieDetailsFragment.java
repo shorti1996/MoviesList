dc1166d761dcb09a02cbe81e89a05eed600ce76e
shorti1996 <shorti1996@gmail.com>
2017-05-07 23:46:05 +0200

    attempting to split app to fragments

--- app/src/main/java/com/liebert/lab002/MovieDetailsFragment.java
+++ app/src/main/java/com/liebert/lab002/MovieDetailsFragment.java
@@ -63,6 +63,7 @@ public class MovieDetailsFragment extends Fragment implements RatingBar.OnRating
         return fragment;
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
